9d3d0c464c036b79fce385d83c5040f0f0968e0d
Jorge Hurtado <jjhurtado796@gmail.com>
2021-01-13 14:52:42 -0500

    Implementacion de los repos

---
+++ src/main/java/com/jobits/pos/reserva/repo/impl/CategoriaRepoImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.reserva.repo.impl;
+
+import com.jobits.pos.reserva.core.domain.Categoria;
+import com.jobits.pos.reserva.core.module.ReservaRepoModule;
+import com.jobits.pos.reserva.core.repo.CategoriaRepo;
+import com.jobits.pos.reserva.repo.entity.CategoriaEntity;
+import com.jobits.pos.reserva.repo.util.ConnectionPool;
+
+public class CategoriaRepoImpl extends JpaCRUDRepository<Categoria, CategoriaEntity>
+        implements CategoriaRepo {
+
+    public CategoriaRepoImpl() {
+        super(ReservaRepoModule.getInstance()
+                .getImplementation(ConnectionPool.class), Categoria.class, CategoriaEntity.class);
+    }
+
+}
---
+++ src/main/java/com/jobits/pos/reserva/repo/impl/UbicacionRepoImpl.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jobits.pos.reserva.repo.impl;
+
+import com.jobits.pos.reserva.core.domain.Ubicacion;
+import com.jobits.pos.reserva.core.module.ReservaRepoModule;
+import com.jobits.pos.reserva.core.repo.UbicacionRepo;
+import com.jobits.pos.reserva.repo.entity.UbicacionEntity;
+import com.jobits.pos.reserva.repo.util.ConnectionPool;
+
+public class UbicacionRepoImpl extends JpaCRUDRepository<Ubicacion, UbicacionEntity>
+        implements UbicacionRepo {
+
+    public UbicacionRepoImpl() {
+        super(ReservaRepoModule.getInstance()
+                .getImplementation(ConnectionPool.class), Ubicacion.class, UbicacionEntity.class);
+    }
+
+}
