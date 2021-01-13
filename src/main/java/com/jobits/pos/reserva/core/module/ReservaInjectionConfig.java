30b6af04e19df524e2f97f23b5ce9a5ac4c76e70
Jorge Hurtado <jjhurtado796@gmail.com>
2021-01-13 14:53:13 -0500

    Injeccion configurada

--- src/main/java/com/jobits/pos/reserva/core/module/ReservaInjectionConfig.java
+++ src/main/java/com/jobits/pos/reserva/core/module/ReservaInjectionConfig.java
@@ -7,8 +7,14 @@ package com.jobits.pos.reserva.core.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import com.jobits.pos.reserva.core.usecase.CategoriaUseCase;
+import com.jobits.pos.reserva.core.usecase.ClienteUseCase;
 import com.jobits.pos.reserva.core.usecase.ReservaUseCase;
+import com.jobits.pos.reserva.core.usecase.UbicacionUseCase;
+import com.jobits.pos.reserva.core.usecase.impl.CategoriaUseCaseImpl;
+import com.jobits.pos.reserva.core.usecase.impl.ClienteUseCaseImpl;
 import com.jobits.pos.reserva.core.usecase.impl.ReservaUseCaseImpl;
+import com.jobits.pos.reserva.core.usecase.impl.UbicacionUseCaseImpl;
 
 /**
  *
@@ -17,11 +23,15 @@ import com.jobits.pos.reserva.core.usecase.impl.ReservaUseCaseImpl;
  * @author Jorge
  *
  */
-public class ReservaInjectionConfig extends AbstractModule {
+class ReservaInjectionConfig extends AbstractModule {
 
     @Override
     protected void configure() {
         bind(ReservaUseCase.class).to(ReservaUseCaseImpl.class).in(Singleton.class);
+        bind(ClienteUseCase.class).to(ClienteUseCaseImpl.class).in(Singleton.class);
+        bind(CategoriaUseCase.class).to(CategoriaUseCaseImpl.class).in(Singleton.class);
+        bind(UbicacionUseCase.class).to(UbicacionUseCaseImpl.class).in(Singleton.class);
+
     }
 
 }
--- src/main/java/com/jobits/pos/reserva/core/module/ReservaRepoInjectionConfig.java
+++ src/main/java/com/jobits/pos/reserva/core/module/ReservaRepoInjectionConfig.java
@@ -9,6 +9,20 @@ import com.google.inject.AbstractModule;
 import com.jobits.pos.reserva.core.usecase.ReservaUseCase;
 import com.jobits.pos.reserva.core.usecase.impl.ReservaUseCaseImpl;
 import com.google.inject.Singleton;
+import com.jobits.pos.reserva.core.repo.CategoriaRepo;
+import com.jobits.pos.reserva.core.repo.ClienteRepo;
+import com.jobits.pos.reserva.core.repo.ReservaRepo;
+import com.jobits.pos.reserva.core.repo.UbicacionRepo;
+import com.jobits.pos.reserva.core.usecase.CategoriaUseCase;
+import com.jobits.pos.reserva.core.usecase.ClienteUseCase;
+import com.jobits.pos.reserva.core.usecase.UbicacionUseCase;
+import com.jobits.pos.reserva.core.usecase.impl.CategoriaUseCaseImpl;
+import com.jobits.pos.reserva.core.usecase.impl.ClienteUseCaseImpl;
+import com.jobits.pos.reserva.core.usecase.impl.UbicacionUseCaseImpl;
+import com.jobits.pos.reserva.repo.impl.CategoriaRepoImpl;
+import com.jobits.pos.reserva.repo.impl.ClienteRepoImpl;
+import com.jobits.pos.reserva.repo.impl.ReservaRepoImpl;
+import com.jobits.pos.reserva.repo.impl.UbicacionRepoImpl;
 
 /**
  *
@@ -21,7 +35,10 @@ class ReservaRepoInjectionConfig extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(ReservaUseCase.class).to(ReservaUseCaseImpl.class).in(Singleton.class);
+        bind(ReservaRepo.class).to(ReservaRepoImpl.class).in(Singleton.class);
+        bind(ClienteRepo.class).to(ClienteRepoImpl.class).in(Singleton.class);
+        bind(CategoriaRepo.class).to(CategoriaRepoImpl.class).in(Singleton.class);
+        bind(UbicacionRepo.class).to(UbicacionRepoImpl.class).in(Singleton.class);
     }
 
 }
