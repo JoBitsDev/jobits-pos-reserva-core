6a5a66dc2079be6446561ecfbfa2af1f7804c8c5
Jorge Hurtado <jjhurtado796@gmail.com>
2021-01-16 21:38:49 -0500

    Bug fix

--- src/main/java/com/jobits/pos/reserva/core/module/ReservaInjectionConfig.java
+++ src/main/java/com/jobits/pos/reserva/core/module/ReservaInjectionConfig.java
@@ -6,7 +6,7 @@
 package com.jobits.pos.reserva.core.module;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Singleton;
+import com.google.inject.Scopes;
 import com.jobits.pos.reserva.core.usecase.CategoriaUseCase;
 import com.jobits.pos.reserva.core.usecase.ClienteUseCase;
 import com.jobits.pos.reserva.core.usecase.ReservaUseCase;
@@ -27,10 +27,10 @@ class ReservaInjectionConfig extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(ReservaUseCase.class).to(ReservaUseCaseImpl.class).in(Singleton.class);
-        bind(ClienteUseCase.class).to(ClienteUseCaseImpl.class).in(Singleton.class);
-        bind(CategoriaUseCase.class).to(CategoriaUseCaseImpl.class).in(Singleton.class);
-        bind(UbicacionUseCase.class).to(UbicacionUseCaseImpl.class).in(Singleton.class);
+        bind(ReservaUseCase.class).to(ReservaUseCaseImpl.class).in(Scopes.SINGLETON);
+        bind(ClienteUseCase.class).to(ClienteUseCaseImpl.class).in(Scopes.SINGLETON);
+        bind(CategoriaUseCase.class).to(CategoriaUseCaseImpl.class).in(Scopes.SINGLETON);
+        bind(UbicacionUseCase.class).to(UbicacionUseCaseImpl.class).in(Scopes.SINGLETON);
 
     }
 
