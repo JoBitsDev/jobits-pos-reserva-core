3da66f2943a4e8b938415ffcc5f7348c57fce4b4
Jorge Hurtado <jjhurtado796@gmail.com>
2021-01-19 14:26:31 -0500

    bug fix reserva

--- src/main/java/com/jobits/pos/reserva/core/usecase/impl/ReservaUseCaseImpl.java
+++ src/main/java/com/jobits/pos/reserva/core/usecase/impl/ReservaUseCaseImpl.java
@@ -100,29 +100,30 @@ public class ReservaUseCaseImpl extends DefaultCRUDUseCase<Reserva> implements R
 
     private boolean validarReserva(Reserva reservaPorValidar) {
         List<Reserva> reservas = repo.findReservasDeDia(reservaPorValidar.getFechareserva());
+        reservas.remove(reservaPorValidar);
         for (int i = 0; i < reservas.size();) {
-            if (reservaPorValidar.getUbicacionidubicacion() != null) {
-                if (reservaPorValidar.getUbicacionidubicacion()
-                        .getEstadoubicacion().equals(UbicacionEstado.INABILITADA.getEstado())) {
-                    throw new IllegalArgumentException(ResourceHandler
-                            .getString("msg.com.jobits.pos.reserva.core.domain.reserva_ubicacion_inhabilitada"));
-                }
-                if (!reservas.get(i).getUbicacionidubicacion().equals(reservaPorValidar.getUbicacionidubicacion())) {
-                    reservas.remove(i);
-                    continue;
-                }
+            if (reservaPorValidar.getUbicacionidubicacion()
+                    .getEstadoubicacion().equals(UbicacionEstado.INABILITADA.getEstado())) {
+                throw new IllegalArgumentException(ResourceHandler
+                        .getString("msg.com.jobits.pos.reserva.core.domain.reserva_ubicacion_inhabilitada"));
+            }
+            if (!reservas.get(i).getUbicacionidubicacion().equals(reservaPorValidar.getUbicacionidubicacion())) {
+                reservas.remove(i);
+                continue;
             }
             if (reservas.get(i).getCategoriaidcategoria().equals(reservaPorValidar.getCategoriaidcategoria())) {
                 if (validarRangoFechas(reservas.get(i), reservaPorValidar)) {
-                    reservas.remove(i);
+                    reservas.remove(0);
+                    continue;
                 } else {
                     throw new IllegalArgumentException(
                             String.format(ResourceHandler
                                     .getString("msg.com.jobits.pos.reserva.core.domain.reserva_fecha_invalida"),
                                     reservas.get(i)));
                 }
+            } else {
+                reservas.remove(0);
             }
-
         }
         reservaPorValidar.validate();
         return reservas.isEmpty();
